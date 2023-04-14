267aef116ceceaf043124de3da9ab23c8b14a71e
shubhidua <shubhidua95577@gmail.com>
2023-04-15 00:14:21 +0530

    nested recyler view

--- app/src/main/java/com/example/collegeapp/ui/faculty/FacultyFragment.java
+++ app/src/main/java/com/example/collegeapp/ui/faculty/FacultyFragment.java
@@ -1,20 +1,149 @@
 package com.example.collegeapp.ui.faculty;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.collegeapp.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FacultyFragment extends Fragment {
+    private RecyclerView csDepartment, eceDepartment, itDepartment;
+    private LinearLayout csNoData, eceNoData, itNoData;
+    private List<TeacherData> list1, list2, list3;
+    private TeacherAdapter adapter;
+    private DatabaseReference reference, dbRef;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_faculty, container, false);
+        View view = inflater.inflate(R.layout.fragment_faculty, container, false);
+
+        csDepartment = view.findViewById(R.id.csDepartment);
+        eceDepartment = view.findViewById(R.id.eceDepartment);
+        itDepartment = view.findViewById(R.id.itDepartment);
+
+        csNoData = view.findViewById(R.id.csNoData);
+        eceNoData = view.findViewById(R.id.eceNoData);
+        itNoData = view.findViewById(R.id.itNoData);
+
+        reference = FirebaseDatabase.getInstance("https://oneclickadminapp-default-rtdb.asia-southeast1.firebasedatabase.app").getReference().child("teacher");
+
+        csDepartment();
+        eceDepartment();
+        itDepartment();
+
+        return view;
     }
+
+    private void csDepartment() {
+        dbRef = reference.child("Computer Science");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list1 = new ArrayList<>();
+//                Log.i("Deepak", list1.toString());
+                if (!snapshot.exists()) {
+                    csNoData.setVisibility(View.VISIBLE);
+                    csDepartment.setVisibility(View.GONE);
+                } else {
+                    csNoData.setVisibility(View.GONE);
+                    csDepartment.setVisibility(View.VISIBLE);
+                    for (DataSnapshot snapshot1: snapshot.getChildren()) {
+                        TeacherData data = snapshot1.getValue(TeacherData.class);
+                        list1.add(data);
+//                        Log.i("Deepak", list1.toString());
+                    }
+                    csDepartment.setHasFixedSize(true);
+                    csDepartment.setLayoutManager(new LinearLayoutManager(getContext()));
+                    adapter = new TeacherAdapter(list1, getContext());
+                    csDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void eceDepartment() {
+        dbRef = reference.child("Electronics and Communication Engineering");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list2 = new ArrayList<>();
+                if (!snapshot.exists()) {
+                    eceNoData.setVisibility(View.VISIBLE);
+                    eceDepartment.setVisibility(View.GONE);
+                } else {
+                    eceNoData.setVisibility(View.GONE);
+                    eceDepartment.setVisibility(View.VISIBLE);
+                    for (DataSnapshot snapshot1: snapshot.getChildren()) {
+                        TeacherData data = snapshot1.getValue(TeacherData.class);
+                        list2.add(data);
+                    }
+                    eceDepartment.setHasFixedSize(true);
+                    eceDepartment.setLayoutManager(new LinearLayoutManager(getContext()));
+                    adapter = new TeacherAdapter(list2, getContext());
+                    eceDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void itDepartment() {
+        dbRef = reference.child("Information Technology");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list3 = new ArrayList<>();
+                if (!snapshot.exists()) {
+                    itNoData.setVisibility(View.VISIBLE);
+                    itDepartment.setVisibility(View.GONE);
+                } else {
+                    itNoData.setVisibility(View.GONE);
+                    itDepartment.setVisibility(View.VISIBLE);
+                    for (DataSnapshot snapshot1: snapshot.getChildren()) {
+                        TeacherData data = snapshot1.getValue(TeacherData.class);
+                        list3.add(data);
+                    }
+                    itDepartment.setHasFixedSize(true);
+                    itDepartment.setLayoutManager(new LinearLayoutManager(getContext()));
+                    adapter = new TeacherAdapter(list3, getContext());
+                    itDepartment.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
