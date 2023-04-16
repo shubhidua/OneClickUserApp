1a4ce1181ef32e0fbbd9700212211f7a3144e091
Deepak Prajapati <dp0774138@gmail.com>
2023-04-16 15:19:00 +0530

    Final touch

--- app/src/main/java/com/example/collegeapp/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/collegeapp/ui/about/AboutFragment.java
@@ -1,5 +1,7 @@
 package com.example.collegeapp.ui.about;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -22,11 +24,14 @@ public class AboutFragment extends Fragment {
     private BranchAdapter adapter;
     private List<BranchModel> list;
 
+    ImageView map;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_about, container, false);
+        map = view.findViewById(R.id.map);
 
         list = new ArrayList<>();
         list.add(new BranchModel(R.drawable.ic_comp, "Computer Science Engineering", "To provide excellent technical education in computer science and engineering and produce competent engineers and professionals with high ethical values prepared for life long learning."));
@@ -36,11 +41,23 @@ public class AboutFragment extends Fragment {
         viewPager = view.findViewById(R.id.viewPager);
         viewPager.setAdapter(adapter);
 
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMap();
+            }
+        });
         ImageView imageView = view.findViewById(R.id.college_image);
         imageView.setImageResource(R.drawable.iiitu_building);
 //        Glide.with(getContext())
 //                .load("https://firebasestorage.googleapis.com/v0/b/oneclickadminapp.appspot.com/o/Notice%2F%5BB%401f780djpg?alt=media&token=b602a0df-2ec0-4cef-b78d-2718bacfa88d")
 //                .into(imageView);
         return view;
+    }
+    private void openMap() {
+        Uri uri = Uri.parse("geo:0, 0?q=Indian Institute of Information Technology (IIIT) Una, Vill. Saloh, Teh. Haroli, Distt, Una, Himachal Pradesh 177209");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.setPackage("com.google.android.apps.maps");
+        startActivity(intent);
     }
 }
